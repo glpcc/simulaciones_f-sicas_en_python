111ab9cd432d2f3486b1abfeaad81ecf2d875617
gonzalo <gonzalolopecc@gmail.com>
2021-09-16 12:11:49 +0200

    introduced collisions but not working correctly

--- ball.py
+++ ball.py
@@ -1,10 +1,11 @@
 import random
+from numpy.lib.function_base import select
 import pygame
 import numpy as np 
 import random
 import math
 
-class ball():
+class Ball():
     def __init__(self,radious,initial_vel,initial_pos):
         self.radious = radious
         self.velocity = initial_vel
@@ -14,19 +15,34 @@ class ball():
         self.position += self.velocity
 
     def draw(self,canvas):
-        pygame.draw.rect(canvas,self.color,self.position,self.radious)
+        pygame.draw.circle(canvas,self.color,self.position,self.radious)
     
     def test_collisions_with_other_balls(self,balls):
         for ball in balls:
-            if (math.sqrt((self.position[0]-ball.position[0])**(2)+(self.position[1]-ball.position[1])) <= self.radious):
+            if np.linalg.norm([ball[0]-self.position[0],ball[1]-self.position[1]]) <= self.radious:
                 self.elastic_collision(ball)
+
+
+
     def test_borders(self,borders):
+        # borders would come in format in side a np array like [[first_point],[border_vector]]
         for border in borders:
-            border_length = np.linalg.norm(border)
-            self.velocity += 2*()
+            border_length = np.linalg.norm(border[1])
+            if self.distance_to_border(border) <= self.radious:
+                if np.linalg.norm([border[0][0]-self.position[0],border[0][1]-self.position[1]])-self.radious <= border_length and np.linalg.norm([(border[0][0]+border[1][0])-self.position[0],(border[0][1]+border[1][1])-self.position[1]])-self.radious <= border_length:
+                    border_perp = np.array([border[1][1],-border[1][0]])
+                    self.velocity -= 2*((np.dot(border_perp,self.velocity)/border_length**(2))*border_perp)
+                    
     def elastic_collision(self,ball):
         self.velocity,ball.velocity = ball.velocity,self.velocity
 
+    def distance_to_border(self,border):
+        border_point2x = border[0][0] + border[1][0]
+        border_point2y = border[0][1] + border[1][1]
+        
+        # use the formula of the distance from a line to a point
+        distance = abs((border_point2y-border[0][0])*(border[0][1]-self.position[1])-(border[0][0]-self.position[0])*(border_point2y-border[0][1]))/np.linalg.norm([border_point2x-border[0][0],border_point2y-border[0][1]])
+        return distance
 
 
         
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
 from pygame.locals import *
 import math
+import numpy as np
+from ball import Ball
 
-canvas = pygame.display.set_mode((1000,1000))
+
+window_height = 800
+window_width = 1000
+canvas = pygame.display.set_mode((window_width,window_height))
 pygame.init()
 runnig = True
 
-
-
-
+walls = np.array([[[0,0],[window_width,0]],[[window_width,0],[0,window_height]],[[window_width,window_height],[-window_width,0]],[[0,window_height],[0,-window_height]]])
+test_ball = Ball(20,np.array([0.1,-1.]),np.array([200.,200.]))
 while runnig:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runnig = False
-    
     canvas.fill((0,0,0))
-            
-
+    test_ball.test_borders(walls)
+    test_ball.update()
+    test_ball.draw(canvas)
     pygame.display.flip()
 
 pygame.quit()
